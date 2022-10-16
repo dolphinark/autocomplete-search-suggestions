9cdac293be6c85f6ed14d1d404343494ceca2695
Rita <ritahung19@gmail.com>
2022-10-16 15:18:52 +0800

    auto suggestions

---
+++ script.js
@@ -0,0 +1,25 @@
+let userInput = document.querySelector("#user-input");
+let searchIcon = document.querySelector("#search-icon");
+let suggestionList = document.querySelector(".suggestion-box");
+
+userInput.onkeyup = (e) => {
+  let userTyped = e.target.value.toLowerCase();
+  let matchedItem;
+
+  if (userTyped) {
+    matchedItem = suggestions.filter((item) => {
+      return item.toLowerCase().startsWith(userTyped);
+    });
+
+    if (matchedItem.length > 0) {
+      let matchedList = matchedItem.map((element) => {
+        return "<li>" + element + "</li>";
+      });
+      suggestionList.innerHTML = matchedList.join("");
+      suggestionList.style.display = "block";
+    }
+  } else {
+    suggestionList.innerHTML = "";
+    suggestionList.style.display = "none";
+  }
+};
